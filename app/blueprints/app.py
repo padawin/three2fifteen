a784d297e4ee60dcf0736e6f99431ebb5eda6a03
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2020-05-01 23:10:33 +0200

    Proxy changed into decorator

--- app/blueprints/app.py
+++ app/blueprints/app.py
@@ -1,21 +1,11 @@
 from flask import Blueprint, request, current_app
-from app.controller.proxy import ProxyController
 from app.controller.user import UserController
 from app.controller.player import PlayerController
-from app.service.proxy import ProxyService
+import app.plugin
 
 
 bp = Blueprint('api', __name__)
 
-
-@bp.route('/api/<string:service>',
-          defaults={'path': ''},
-          methods=ProxyService.valid_methods.keys())
-@bp.route('/api/<string:service>/<path:path>',
-          methods=ProxyService.valid_methods.keys())
-def proxy(service, path):
-    c = ProxyController(current_app.config)
-    return c.execute(True, request, service, "/{}".format(path))
 
 # Auth routes
 
@@ -33,6 +23,7 @@ def login():
 # Social routes
 
 @bp.route('/player/names/<string:id_users>', methods=['GET'])
-def get_player_names(id_users):
+@app.plugin.need_auth
+def get_player_names(id_users, identity):
     controller = PlayerController()
     return controller.get_names(request, id_users.split(','))
