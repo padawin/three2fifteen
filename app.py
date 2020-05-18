f641954c564be762f2903b9b21ab6c8e3f35d856
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2020-05-18 17:56:19 +0200

    passenger_wsgi.py added

---
+++ app.py
@@ -0,0 +1,81 @@
+import logging
+from flask import Flask, current_app, abort
+from flask_cors import CORS
+from werkzeug.utils import find_modules, import_string
+from app.model import model
+import http.client as http_client
+
+
+def register_logger():
+    http_client.HTTPConnection.debuglevel = 1
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.DEBUG)
+    requests_log = logging.getLogger("requests.packages.urllib3")
+    requests_log.setLevel(logging.DEBUG)
+    requests_log.propagate = True
+
+
+def register_cors(app):
+    CORS(app, origins=app.config['WEB_HOST'])
+
+
+def register_setup(app):
+    @app.before_request
+    def get_db():
+        logging.info("Creating DB connection")
+        try:
+            model.Model.connect("dbname={} user={} host={} password={}".format(
+                current_app.config['DATABASE_NAME'],
+                current_app.config['DATABASE_USER'],
+                current_app.config['DATABASE_HOST'],
+                current_app.config['DATABASE_PASSWORD']
+            ))
+        except model.ConnectionError:
+            logging.error("Failed connecting to database {} at {}".format(
+                current_app.config['DATABASE_NAME'],
+                current_app.config['DATABASE_HOST']
+            ))
+            abort(500)
+
+
+def register_blueprints(app):
+    """Register all blueprint modules
+
+    Reference: Armin Ronacher, "Flask for Fun and for Profit" PyBay 2016.
+    """
+    for name in find_modules('app.blueprints'):
+        mod = import_string(name)
+        try:
+            blueprint = mod.bp
+        except AttributeError:
+            pass
+        else:
+            app.register_blueprint(blueprint)
+    return None
+
+
+def register_teardowns(app):
+    @app.teardown_appcontext
+    def close_db(error):
+        logging.info("Disconnecting DB connection")
+        model.Model.disconnect()
+
+
+app = Flask('three2fifteen-api')
+
+app.config.update({})
+app.config.from_envvar('THREE2FIFTEEN_API_SETTINGS', silent=False)
+
+logger = logging.getLogger("{} logger".format(app.name))
+logger.setLevel(logging.DEBUG)
+logging.info("starting server")
+
+register_logger()
+register_cors(app)
+register_setup(app)
+register_blueprints(app)
+register_teardowns(app)
+
+
+if __name__ == "__main__":
+    app.run()
